f9407daa5f7c16a04645ebe8d8891f7977a32ea1
Michał Nowakowski <nowakowskimichal1988@gmail.com>
2019-07-07 21:11:26 +0200

    Change script.js 4

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@
 //(function() {
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
-//var prefix = "https://cors-anywhere.herokuapp.com/";
+var prefix = "https://cors-anywhere.herokuapp.com/";
 
 function getQuote() {
-    fetch(quoteUrl, { cache: "no-store" })
+    fetch(prefix + quoteUrl, { cache: "no-store" })
         .then(function(resp) {
             return resp.json();
         })
