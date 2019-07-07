1607ca36430d3fb87f0ff5f6911c755c026d3a7f
Michał Nowakowski <nowakowskimichal1988@gmail.com>
2019-07-07 21:09:29 +0200

    Change script.js 3

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 "use strict";
 
-(function() {
+//(function() {
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
-var prefix = "https://cors-anywhere.herokuapp.com/";
+//var prefix = "https://cors-anywhere.herokuapp.com/";
 
 function getQuote() {
-    fetch(prefix + quoteUrl, { cache: "no-store" })
+    fetch(quoteUrl, { cache: "no-store" })
         .then(function(resp) {
             return resp.json();
         })
         .then(createTweet);
-};
+}
 
 function createTweet(input) {
     var data = input[0];
@@ -35,7 +35,7 @@ function createTweet(input) {
     	document.querySelector('.author').innerText = "Author: " + quoteAuthor;
     	document.querySelector('.tweet').setAttribute('href', tweet);
 	}
-};
+}
 
 document.addEventListener('DOMContentLoaded', function() {
     getQuote();
@@ -43,4 +43,4 @@ document.addEventListener('DOMContentLoaded', function() {
         getQuote();
     });
 });
-})();
+//})();
